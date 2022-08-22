6b9a41fe382c1fce8971842478f3b5265d47b434
FavourOyewumi7 <oyewumifavour65@gmail.com>
2022-08-22 11:19:04 +0100

    how far na new stuff be this

---
+++ country_quiz/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+ 
+  <HomeContainer />
+ 
+</template>
+
+<script>
+
+import HomeContainer from './components/HomeContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+   
+    HomeContainer
+}
+}
+</script>
+
+<style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../src/assets/background.png);
+  background-position:center;
+  background-repeat: no-repeat;
+ 
+}
+</style>
